18b1e13345cd092a0ce494d2f14a2c9f320eb88a
Thomas Weigert <thomasweigert91@gmail.com>
2022-10-16 13:17:50 +0200

    First Commit all files

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const filter_btns = document.querySelectorAll(".filter-btn");
+
+filter_btns.forEach((btn) => {
+btn.addEventListener('click', () => {
+  filter_btns.forEach((button) => button.classList.remove("active"));
+btn.classList.add("active");
+
+let filtervalue = btn.dataset.filter;
+
+$(".grid").isotope ({filter : filtervalue});
+
+})
+});
+
+
+
+$('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    transitionDuration: '0.6s' ,
+  });
